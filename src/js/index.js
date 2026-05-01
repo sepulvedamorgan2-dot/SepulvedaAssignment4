2f19d67ebed8ae0b36cccafdb7b11ca6745ee296
mepul <sepulvedamorgan2@gmail.com>
2026-05-01 15:26:30 -0500

    1.2 Pre Alert Attempt

--- src/js/index.js
+++ src/js/index.js
@@ -15,7 +15,7 @@ const addFormEle = document.getElementById('addtaskform');
 let taskId = 0
 class Task {
     constructor(title, description, category, isCompleted = false) {
-        this.id = ++taskId
+        this.id = Number(++taskId)
         this.title = title
         this.description = description
         this.category = category
@@ -24,12 +24,12 @@ class Task {
 }
 const tasks = [
     new Task('Clean kitchen', 'Wash dishes and wipe counters', 'Chore', false),
-    new Task('Laundry', 'Wash, dry, and fold clothes', 'Chore', false),
+    new Task('Laundry', 'Wash, dry, and fold clothes', 'Chore', true),
     new Task('Take out trash', 'Empty bins and replace liners', 'Chore', false),
-    new Task('Prep lunches', 'Make meals for the week', 'Meal Prep', false),
-    new Task('Cook dinner', 'Prepare a balanced meal', 'Meal Prep', false),
-    new Task('Freeze snacks', 'Portion out snacks for later', 'Meal Prep', false),
-    new Task('Math assignment', 'Finish chapter 5 problems', 'Homework', false),
+    new Task('Prep lunches', 'Make meals for the week', 'Mealprep', false),
+    new Task('Cook dinner', 'Prepare a balanced meal', 'Mealprep', true),
+    new Task('Freeze snacks', 'Portion out snacks for later', 'Mealprep', false),
+    new Task('Math assignment', 'Finish chapter 5 problems', 'Homework', true),
     new Task('Read history', 'Review notes for quiz', 'Homework', false),
     new Task('Change air filter', 'Replace HVAC filter', 'Maintenance', false),
     new Task('Check smoke detectors', 'Test batteries and functionality', 'Maintenance', false)
@@ -37,7 +37,7 @@ const tasks = [
 
 addFormEle.addEventListener('submit', function (e) {
     e.preventDefault();
-    if(addcategoryEle.value === ''){
+    if (addcategoryEle.value === '') {
         alert('Please Select a category')
         return;
     }
@@ -47,29 +47,108 @@ addFormEle.addEventListener('submit', function (e) {
     this.reset();
 
 })
+
+filterCategoryEle.addEventListener('change', filterTasks);
+filtertitleEle.addEventListener('keydown', filterTasks);
+filtercompletedEle.addEventListener('change', filterTasks);
+
+
 function filterTasks() {
+
+    renderAreaEle.innerHTML = '';
+    const filterCategory = filterCategoryEle.value;
+    const filterTitle = filtertitleEle.value;
+    const filterCompleted = filtercompletedEle.value;
+    console.log(filterTitle)
     tasks.forEach(task => {
-        renderTask(task)
+        let shouldRender = true;
+
+        if (!(filterCategory == 'all' || filterCategory === task.category)) {
+            shouldRender = false;
+        }
+        if (!(filterTitle === '' || String(task.title).toLowerCase().includes(filterTitle.toLowerCase()))) {
+            shouldRender = false;
+        }
+        if (!(filterCompleted === 'all' || filterCompleted === String(task.isCompleted))) {
+            shouldRender = false;
+        }
+
+        if (shouldRender) {
+            renderTask(task);
+        }
 
     });
 }
 function renderTask(task) {
     const eleToAdd = document.createElement('div')
-    eleToAdd.innerHTML = `<div data-id="${task.id}" class="taskcard">
+    eleToAdd.innerHTML =
+        `<div data-id="${task.id}" class="taskcard">
             <img src="/src/images/${task.category}.jpg" alt="">
-                <div id="test>
-                    <section class="row">
+                <div id="test">
+                    <section id="test2">
                         <h3>${task.title}</h3>
-                        <h3 id="mediaDel"> / </h3>
                         <h3 id="mediaShrink">${task.category}</h3>
                     </section>
                     <p>${task.description}</p>
                 </div>
                 <section class="icon">
-                    <p><i class="bi bi-check-square-fill"></i> </p>
+                    <p><i class="bi bi-check-square-fill ${task.isCompleted ? 'completed' : 'not-completed'}"></i><i class="bi bi-trash3-fill"></i> </p>
                 </section>
         </div>`
     renderAreaEle.innerHTML += eleToAdd.innerHTML;
 }
+
+document.getElementById('outputarea').addEventListener('click', function (e) {
+    const idd = Number(e.target.closest('.taskcard').dataset.id)
+    console.log(idd)
+    if (e.target.classList.contains('bi-trash3-fill')) {
+        console.log('no')
+        const pop = tasks.findIndex(function (task) {
+            if (task.id === idd) {
+                return true
+            }
+            return false
+        })
+        if (confirm(`Are you sure you want to delete the task: ${tasks[pop].title}`)) {
+            tasks.splice(pop, 1)
+            filterTasks()
+            return
+        }
+
+    }
+
+    if (e.target.classList.contains('not-completed')) {
+        console.log('no')
+        const test = tasks.filter(function (task, i) {
+
+            if (task.id === idd) {
+                task.isCompleted = true
+                filterTasks()
+                return true
+
+            }
+            return false
+        })
+        
+
+    }
+    if (e.target.classList.contains('completed')) {
+        console.log('no')
+        const test = tasks.filter(function (task, i) {
+
+            if (task.id === idd) {
+                task.isCompleted = false
+                filterTasks()
+                return true
+
+            }
+            return false
+        })
+
+
+    }
+
+})
+
 
 filterTasks()
