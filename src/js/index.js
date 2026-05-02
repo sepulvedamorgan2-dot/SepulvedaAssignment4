589692bc4a1446f492b242da063451d610170be1
mepul <sepulvedamorgan2@gmail.com>
2026-05-02 10:03:33 -0500

    1.5 Bug fixes

--- src/js/index.js
+++ src/js/index.js
@@ -13,7 +13,8 @@ const adddescriptionEle = document.getElementById('description');
 //forms
 const filterFormEle = document.getElementById('filterform');
 const addFormEle = document.getElementById('addtaskform');
-//alert
+//demo list
+const demoListEle = document.getElementById('demoList');
 
 
 
@@ -27,7 +28,7 @@ class Task {
         this.isCompleted = isCompleted
     }
 }
-const tasks = returnLocalStorage();
+let tasks = returnLocalStorage();
 
 addFormEle.addEventListener('submit', function (e) {
 
@@ -159,5 +160,19 @@ document.getElementById('outputarea').addEventListener('click', function (e) {
 
 })
 
+demoListEle.addEventListener('click', function (e) {
 
+    tasks = [new Task('Clean kitchen', 'Wash dishes and wipe counters', 'Chore', false),
+    new Task('Laundry', 'Wash, dry, and fold clothes', 'Chore', true),
+    new Task('Take out trash', 'Empty bins and replace liners', 'Chore', false),
+    new Task('Prep lunches', 'Make meals for the week', 'Mealprep', false),
+    new Task('Cook dinner', 'Prepare a balanced meal', 'Mealprep', true),
+    new Task('Freeze snacks', 'Portion out snacks for later', 'Mealprep', false),
+    new Task('Math assignment', 'Finish chapter 5 problems', 'Homework', true),
+    new Task('Read history', 'Review notes for quiz', 'Homework', false),
+    new Task('Change air filter', 'Replace HVAC filter', 'Maintenance', false),
+    new Task('Check smoke detectors', 'Test batteries and functionality', 'Maintenance', false),
+    new Task('Test LocalStorage', 'Sort and clean storage area', 'Maintenance', true)]
+    filterTasks()
+})
 filterTasks()
--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -13,17 +13,7 @@ export function returnLocalStorage() {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
         tasks = [
-            new Task('Clean kitchen', 'Wash dishes and wipe counters', 'Chore', false),
-            new Task('Laundry', 'Wash, dry, and fold clothes', 'Chore', true),
-            new Task('Take out trash', 'Empty bins and replace liners', 'Chore', false),
-            new Task('Prep lunches', 'Make meals for the week', 'Mealprep', false),
-            new Task('Cook dinner', 'Prepare a balanced meal', 'Mealprep', true),
-            new Task('Freeze snacks', 'Portion out snacks for later', 'Mealprep', false),
-            new Task('Math assignment', 'Finish chapter 5 problems', 'Homework', true),
-            new Task('Read history', 'Review notes for quiz', 'Homework', false),
-            new Task('Change air filter', 'Replace HVAC filter', 'Maintenance', false),
-            new Task('Check smoke detectors', 'Test batteries and functionality', 'Maintenance', false),
-            new Task('Test LocalStorage', 'Sort and clean storage area', 'Maintenance', true)
+            
         ];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
